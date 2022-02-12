75c32e2598164e42dac5ceea623f96ab1998e322
lau-com <catagarco@gmail.com>
2022-02-12 09:39:18 -0500

    multiplicacion

--- Funciones.py
+++ Funciones.py
@@ -2,4 +2,11 @@ def Resta(x,y):
     resultado=x-y
     return resultado
 
-print(Resta(28-17))
+print(Resta(28,17))
+
+def Multiplicacion():
+    x=imput("x")
+    y=imput("y")
+    resultado=x*y
+    print(resultado)
+    return resultado
