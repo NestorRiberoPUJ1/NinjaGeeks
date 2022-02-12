7df180fc6858335601004ae1153bcbf895679814
gabriela Morales <gabimorales@gmail.com>
2022-02-12 09:41:28 -0500

    Update Funciones.py

--- Funciones.py
+++ Funciones.py
@@ -2,4 +2,13 @@ def Resta(x,y):
     resultado=x-y
     return resultado
 
-print(Resta(28-17))
+print(Resta(28,17))
+#division
+def division():
+    a=float(input("a = "))
+    b=float(input("b = "))
+    resultado= a/b
+    return resultado 
+
+DIVISION=division()
+print("resultado = "+ str(DIVISION))
