5fedd725ac9025f3bb59bbfd29e70245625615a4
catalinaguevara <catalina.guevara.lemus@gmail.com>
2022-02-17 18:50:49 -0500

    Update Funciones.py

--- Funciones.py
+++ Funciones.py
@@ -1,26 +1,26 @@
-#resta
+#resta      params: x->num  y->num
 def Resta(x,y):
     resultado=x-y
     return resultado
 
-#multiplicacion
+#multiplicacion params: x->num  y->num
 def Multiplicacion(x,y):
     resultado=x*y
     return resultado
 
-#division
+#division   params: a->num  b->num
 def division(a,b):
     resultado= a/b
     return resultado 
 
-#suma
+#suma   params: x->num  y->num
 def Suma(x,y):
     resultado=x+y
     return resultado
 
-#sumatoria
+#sumatoria  params: lista-> list[num]
 def sumatoria (lista):
     resultado=0
     for index in lista:
         resultado= resultado+index 
-    return (resultado)
+    return resultado
