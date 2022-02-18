9bb9e6d59b9b3f30d4581e0020a5cf7f0ed82996
catalinaguevara <catalina.guevara.lemus@gmail.com>
2022-02-17 19:51:51 -0500

    funciones
    se corrigieron nombre  se agrego la funcion promedio

--- Funciones.py
+++ Funciones.py
@@ -1,27 +1,34 @@
-#suma   params: x->num  y->num
+#suma   params: sumando1->num  sumando2->num
 def Suma(sumando1:int,sumando2:int):
     resultado=sumando1+sumando2
     return resultado 
 
-#resta      params: x->num  y->num
+#resta      params: minuendo->num  sustraendo->num
 def Resta(minuendo:int,sustraendo:int):
     resultado=minuendo-sustraendo
     return resultado
 
-#multiplicacion params: x->num  y->num
+#multiplicacion params: factor1->num  factor2->num
 def Multiplicacion(factor1:int,factor2:int):
     resultado=factor1*factor2
     return resultado
 
-#division   params: a->num  b->num
-def division(dividendo:int,divisor:int):
+#division   params: dividendo->num  divisor->num
+def Division(dividendo:int,divisor:int):
     resultado= dividendo/divisor
     return resultado 
 
 
 #sumatoria  params: lista-> list[num]
-def sumatoria (lista:list):
+def Sumatoria (lista:list):
     resultado=0
     for index in lista:
         resultado= resultado+index 
     return int(resultado)
+
+#promedio 
+def Promedio (lista:list):
+    promedio=0
+    longitud=len(lista)
+    promedio=Sumatoria(lista)/longitud
+    return promedio 
--- ejemplo.py
+++ ejemplo.py
@@ -1,11 +1,15 @@
 import Funciones
 
-lista =[10,45,-7,8,87,-10,80,-230,-465,45] 
+lista =[8,9,7,10,56] 
 colores= [13,45,65,-40,-34,30]
 
-print(Funciones.sumatoria(colores))
+print(Funciones.Sumatoria(colores))
 print (Funciones.Suma(4,17))
 print (Funciones.Resta(89,40))
-print (Funciones.division(60,7))
+print (Funciones.Division(60,7))
 print (Funciones.Multiplicacion(20,6))
 print (Funciones.Resta(4,6))
+
+print (Funciones.Multiplicacion(6,Funciones.Division(Funciones.Resta(8,7),Funciones.Suma(6,6))))
+
+print (Funciones.Promedio(lista))
