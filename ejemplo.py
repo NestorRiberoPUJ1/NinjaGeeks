b32888066bf75cb4e3d0c89eedfd0b5ac06f3b69
Francisco Carlos Calderon <nestor_05ribero@hotmail.com>
2022-02-19 10:04:52 -0500

    Update

--- ejemplo.py
+++ ejemplo.py
@@ -1,9 +1,16 @@
+from cgi import print_directory
 import Funciones
 
-operacion=input("Ingrese operacion ")
 
+operacion=input("Ingrese operacion ")
+index=0
 for x in operacion:
     if(x=="+"):
         print("ES SUMA")
+        N1=int(operacion[:index])
+        N2=int(operacion[index+1:])
+        print(Funciones.Suma(N1,N2))
+    index=index+1
 
 #################################
+
