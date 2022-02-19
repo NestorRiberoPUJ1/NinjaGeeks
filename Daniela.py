5655085821266e2657c3d6ebe92411b1f98f33be
DanielaEspitia25 <danilaquilla244@gmail.com>
2022-02-19 09:23:15 -0500

    Daniela

---
+++ Daniela.py
@@ -0,0 +1,11 @@
+import Funciones
+
+def producto(a,b):
+    res=0 
+    for x in range (abs(b)):
+        if (b>0):
+            res+= Funciones.Suma(res,a)
+        if (b<0):
+            res+= Funciones.Resta (res,a)
+    return res 
+print (producto(5,4))
