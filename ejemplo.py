ddefe4fda2567efc7750dbb98160b5f0189ffb72
Francisco Carlos Calderon <nestor_05ribero@hotmail.com>
2022-02-22 19:49:44 -0500

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -3,14 +3,16 @@ import Funciones
 
 
 operacion=input("Ingrese operacion ")
-index=0
-for x in operacion:
-    if(x=="+"):
-        print("ES SUMA")
-        N1=int(operacion[:index])
-        N2=int(operacion[index+1:])
-        print(Funciones.Suma(N1,N2))
-    index=index+1
+
+def nombreFuncion(operacion):
+    index=0
+    for x in operacion:
+        if(x=="+"):
+            print("ES SUMA")
+            N1=int(operacion[:index])
+            N2=int(operacion[index+1:])
+            print(Funciones.Suma(N1,N2))
+        index=index+1
 
 #################################
 
