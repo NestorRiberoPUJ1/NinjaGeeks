c8c3b14a7c97793c96fd1d2594c1f4697b227855
Francisco Carlos Calderon <nestor_05ribero@hotmail.com>
2022-03-05 08:54:05 -0500

    create dicts

--- Diccionarios/Cata_Dict.py
+++ Diccionarios/Cata_Dict.py
@@ -1,53 +1,53 @@
-from termcolor import cprint  
+from termcolor import cprint
 
-cata= {"nombre":"catalina", 
+cata = {"nombre": "catalina",
         "apellido": "guevara",
-        "estatura":1.58,
+        "estatura": 1.58,
         "color favorito": "agua marina",
         "edad": 14,
-        "materia favorita":"danzas",
-        "profesor mas rata": "maop",}
-cata.update ({"profesor favorito":"ninguno"})
- 
-
-cprint (cata["nombre"],"magenta")
-cprint (cata["edad"],"blue")
-cprint (cata["profesor mas rata"],"red")
-cprint (cata["profesor favorito"],"blue")
-
-for x in cata: 
-    print (f"{x}:{cata[x]}")
-
-hola= "HoLa a ToDos LlEgUe Yo La DiVaZa"
-cprint (hola.upper(),"blue")
-cprint (hola.lower(),"magenta")
-cprint (hola.title(),"cyan")
-
-for x in cata: 
-    cprint (f"{x}:{str(cata[x]).upper()}","blue")
-for x in cata: 
-    cprint (f"{x}:{str(cata[x]).lower()}","cyan")
-for x in cata: 
-    cprint (f"{x}:{str(cata[x]).title()}","green")
+        "materia favorita": "danzas",
+        "profesor mas rata": "maop", }
+cata.update({"profesor favorito": "ninguno"})
 
 
-malala= {"nombre":"alejandra", 
-        "apellido": "avila",
-        "estatura":1.61,
-        "color favorito": "nose",
-        "edad": 14,
-        "materia favorita":"danzas",
-        "profesor mas rata": "maop",}
+cprint(cata["nombre"], "magenta")
+cprint(cata["edad"], "blue")
+cprint(cata["profesor mas rata"], "red")
+cprint(cata["profesor favorito"], "blue")
 
-contactos= []
+for x in cata:
+    print(f"{x}:{cata[x]}")
+
+hola = "HoLa a ToDos LlEgUe Yo La DiVaZa"
+cprint(hola.upper(), "blue")
+cprint(hola.lower(), "magenta")
+cprint(hola.title(), "cyan")
+
+for x in cata:
+    cprint(f"{x}:{str(cata[x]).upper()}", "blue")
+for x in cata:
+    cprint(f"{x}:{str(cata[x]).lower()}", "cyan")
+for x in cata:
+    cprint(f"{x}:{str(cata[x]).title()}", "green")
+
+
+malala = {"nombre": "alejandra",
+          "apellido": "avila",
+          "estatura": 1.61,
+          "color favorito": "nose",
+          "edad": 14,
+          "materia favorita": "danzas",
+          "profesor mas rata": "maop", }
+
+contactos = []
 contactos.append(cata)
 contactos.append(malala)
-print (contactos)
-cprint (contactos[0],"green")
-cprint (contactos[0]["nombre"],"red")
+print(contactos)
+cprint(contactos[0], "green")
+cprint(contactos[0]["nombre"], "red")
 
-diccionario={"cata":cata}
-diccionario.update ({"malala":malala})
-print (diccionario)
-cprint (diccionario["cata"],"green")
-cprint (diccionario["malala"]["nombre"],"red")
+diccionario = {"cata": cata}
+diccionario.update({"malala": malala})
+print(diccionario)
+cprint(diccionario["cata"], "green")
+cprint(diccionario["malala"]["nombre"], "red")
