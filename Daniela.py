68e4d37ff83f1fdd6280480cdec4225242bcd0a0
DanielaEspitia25 <danilaquilla244@gmail.com>
2022-03-05 08:10:35 -0500

    Update Daniela.py

--- Daniela.py
+++ Daniela.py
@@ -1,3 +1,5 @@
+from re import X
+from statistics import _NumberT
 import Funciones
 
 def producto(a,b):
@@ -13,13 +15,38 @@ print (producto(5,4))
 #############################################
 
 operacion=input("Ingresa operacion ")
-
+flag= False
+Num1=""
+Num2=""
 for x in operacion:
     if(x=="+"):
-        print("ES SUMA")
-    if(x=="-"):
-        print("ES RESTA")
-    if(x=="*"):
-        print("ES MULTIPLICACION")
-    if (x=="/"):
-        print("ES DIVISION")
+        operador = "SUMA"
+        flag=True
+    elif(x=="-"):
+        operador = "RESTA"
+        flag=True
+    elif(x=="*"):
+        operador = "MULTIPLICACION"
+        flag=True
+    elif (x=="/"):
+        operador = "DIVISION"
+        flag=True
+    else:
+        if(flag == False):
+           Num1 = Num1 + X
+        elif (flag == True):
+           Num2 = Num2 + X
+print (Num1)
+Num1= float (Num1)
+print (operador)
+print (Num2)
+Num2= float (Num2)
+
+if (operador == "+"):
+    print ("="+ str (Num1 +Num2))
+elif (operador == "-"):
+    print ("=" + str  (Num1-Num2))
+elif (operador == "*"):
+    print ("=" + str  (Num1*Num2))
+elif (operador == "/"):
+    print ("=" + str  (Num1/Num2))
