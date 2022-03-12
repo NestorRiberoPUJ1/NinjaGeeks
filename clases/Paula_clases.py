3bd5994a1b1f8d6c6245618a4b4f05fb9b674004
Vale-com <valegarco18@gmail.com>
2022-03-12 09:24:34 -0500

    clasess

--- clases/Paula_clases.py
+++ clases/Paula_clases.py
@@ -1,17 +1,20 @@
+from termcolor import colored, cprint
 
 class auto:
-    def __init__(self):
-        self.marca=""
-        self.modelo=""
-        self.año=""
-        self.color=""
-        self.automatico=""
-        self.ventanas=""
-        self.pais=""
-        self.puertas=""
-        self.sillas=""
+    def __init__(self, marca, modelo, año, color, automatico, ventanas, pais, puertas, sillas):
+        self.marca= marca
+        self.modelo= modelo
+        self.año= año
+        self.color= color
+        self.automatico= automatico
+        self.ventanas= ventanas
+        self.pais= pais
+        self.puertas= puertas
+        self.sillas= sillas
 
-DaniMovil= auto()
-print(DaniMovil)
+PaulaMovil = auto("BMW","gran copupé","2021","gris",True,5,"Alemania",4,5)
+cprint(PaulaMovil, "red")
 
+LauraMovil = auto("ferrari","enzo","2016","negro",True,4,"Italia",2,4)
+cprint(LauraMovil, "cyan")
 
