1bc6b499039b445d8ca4c0bafb37eccea5da8cf4
gabriela Morales <99082601+Gabriela787@users.noreply.github.com>
2022-03-12 08:46:32 -0500

    gaby

---
+++ clases/Gaby_clases.py
@@ -0,0 +1,16 @@
+from termcolor import colored, cprint
+
+class auto:
+
+    def __init__(self):
+        self.marca=""
+        self.año=""
+        self.color=""
+        self.automatico=""
+        self.ventanas=""
+        self.sillas=""
+        self.airbags=""
+        self.puertas=""
+
+GabyMovil=auto()
+cprint(GabyMovil, "magenta")
