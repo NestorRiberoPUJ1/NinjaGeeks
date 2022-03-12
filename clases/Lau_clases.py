c48daf3512c7a9f98a20a9557de53b43bc0cd57f
lau-com <catagarco@gmail.com>
2022-03-12 08:48:58 -0500

    UPDATE

---
+++ clases/Lau_clases.py
@@ -0,0 +1,13 @@
+##clase
+class auto:
+    def __init__(self):
+        self.marca="toyota"
+        self.modelo="prado"
+        self.año="2020"
+        self.color="rojo"
+        self.sunrorf="true"
+        self.pais="japon"
+        self.puestos="5"
+
+LauMovil=auto()
+print(LauMovil)
